ca5a699b40cddf52a5de8ac7a5c5920f86a71c2f
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-03-15 20:42:29 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 =2
+const multiply= (num1*num)
 
-// Write your code here
+let num3 = 29
+let num 4 = 5
+let mod = (num3%= num4)
+
+const random = 5
+
+let max = 20
+max.math(0,20)
